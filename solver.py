0565022c7991659217f585a2ff6c71167ffbc7d0
Carson DeSotel <carson.desotel@gmail.com>
2021-01-11 16:49:18 -0600

    Works when a choice is not required

--- solver.py
+++ solver.py
@@ -1,35 +1,22 @@
 from board import Board 
 from board import _SIZE
 import math 
+
 def _solver(x):
-  x.pretty_print()
-  x.generate_possibilities() # don't forget: needs to be seeded
-  
-  # remove 1 element sets
-  for i in range(x.size):
-    for j in range(x.size):
-      if len(x.board[i][j].guess) == 1:
-        t = x.board[i][j].guess.pop()
-        x.board[i][j].value = t
-        x.board[i][j].is_clue = True
-        x.regenerate_possibilities(t, math.floor(i / 3), math.floor(j / 3))
-  
-  x.generate_possibilities()
-
-  for i in range(x.size):
-    for j in range(x.size):
-      if len(x.board[i][j].guess) == 1:
-        t = x.board[i][j].guess.pop()
-        x.board[i][j].value = t
-        x.board[i][j].is_clue = True
-        x.regenerate_possibilities(t, math.floor(i / 3), math.floor(j / 3))
-
-
-  print(x.board[6][1].guess, x.board[6][1].value, x.board[6][1].is_clue)
-  x.pretty_print()
-
+  while not x.is_valid():
+    for i in range(_SIZE):
+      for j in range(_SIZE):
+        if len(x.board[i][j].guess) == 1:
+          t = x.board[i][j].guess.pop()
+          x.board[i][j].value = t
+          x.regenerate_possibilities(t, i, j)
 
 def solve(filename):
-  x = Board(9)
+  x = Board()
   x.read_from_file(filename)
+  print("CLUES:")
+  x.pretty_print()
   _solver(x)
+  print("SOLVED")
+  x.pretty_print()
+  print("VALID:", x.is_valid())
